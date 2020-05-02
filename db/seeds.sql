6d9c9e0ece44b3060df845d4aa79e9c0fd453145
sbayreder <sbayreder@gmail.com>
2020-05-01 22:47:58 -0500

    file creation

---
+++ db/seeds.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+INSERT INTO burgers (burger_name , devoured)VALUES ('Cheese Burger' , false);
+INSERT INTO burgers (burger_name , devoured)VALUES ('Bacon Burger', false);
+INSERT INTO burgers (burger_name , devoured)VALUES ('Whiskey Mushroom Burger' , false);
+INSERT INTO burgers (burger_name , devoured)VALUES ('Hawaiian Burger' , false);
+INSERT INTO burgers (burger_name , devoured)VALUES ('Hamburger Burger', false);
+INSERT INTO burgers (burger_name , devoured)VALUES ('Beyond Burger' , false);
+INSERT INTO burgers (burger_name , devoured)VALUES ('Mushroom Onion Burger' , false);
+INSERT INTO burgers (burger_name , devoured)VALUES ('Heart Stopper Burger' , false);
+
